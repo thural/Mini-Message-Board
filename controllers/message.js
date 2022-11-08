1defeee2c061b1929f5ff4ff5b2a19cf729065ce
Thural Musaibov <tural.musaibov@outlook.com>
2022-11-08 13:51:46 +0400

    small refactoring on controllers

--- controllers/message.js
+++ controllers/message.js
@@ -12,24 +12,24 @@ const { body, validationResult } = require("express-validator");
 // Display list of all Messages.
 exports.board = (req, res, next) => {
   Message.find()
-    .sort([["date", "descending"]])
-    .exec((err, messages) => {
-      if (err) return next(err);
-      if (req.user) { // if user logged in bring user's post to the top
-        const postIndex = messages.findIndex(elem => elem.username === req.user.username);
-        if (postIndex !== -1){
-        const userPost = messages[postIndex];
-        messages.splice(postIndex, 1);
-        messages.unshift(userPost);
-        }
-      };
-      //Successful, so render
-      res.render("message_board", {
-        title: "Message Board",
-        messages,
-        user: req.user
-      });
+  .sort([["date", "descending"]])
+  .exec((err, messages) => {
+    if (err) return next(err);
+    if (req.user) { // if user logged in bring user's post to the top
+      const postIndex = messages.findIndex(elem => elem.username === req.user.username);
+      if (postIndex !== -1){
+      const userPost = messages[postIndex];
+      messages.splice(postIndex, 1);
+      messages.unshift(userPost);
+      }
+    }
+    //Successful, so render
+    res.render("message_board", {
+      title: "Message Board",
+      messages,
+      user: req.user
     })
+  })
 };
 
 // Display message create form on GET.
@@ -40,18 +40,8 @@ exports.create_get = (req, res, next) => {
   })
 };
 
-// Display message edit form on PATCH.
-exports.edit_get = (req, res, next) => {
-  Message.findOne({"username": req.user.username}).exec((err, message) =>{
-    res.render("message_form", {
-      title: "Edit Message",
-      data: { name: req.user.username, message: message.message }
-    })
-  })
-};
-
-// Handle message edit on PATCH.
-exports.edit_post = [
+// Handle message create on POST.
+exports.create_post = [
   // Validate and sanitize the name field.
   body("message", "at least 2 characters required").isLength({ min: 2 }),
   body("message", "max 64 characters allowed").isLength({ max: 64 }),
@@ -61,18 +51,80 @@ exports.edit_post = [
     const does_match = dirty_words.some(word => {
       const regex = new RegExp(`\\s${word}\\s|mother.+|sister.+`, 'i');
       return regex.test(value)
-    });
-    if (does_match) return false;
-    return true;
+    })
+    if (does_match) return false
+    return true
   }).withMessage("Your message can not contain bad words"),
 
   // Process request after validation and sanitization.
   (req, res, next) => {
     // Extract the validation errors from a request.
     const errors = validationResult(req);
+    // Create a message object with escaped and trimmed data.
+    const message = new Message({ username: req.user.username, message: req.body.message })
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("message_form", {
+        title: "Create Message",
+        method: 'POST',
+        data: message,
+        errors: errors.array(),
+      })
+      return
+    } else {
+      // Data from form is valid.
+      // Check if message with same name and message already exists.
+      Message.findOne({ username: req.user.username, message: req.body.message })
+      .exec((err, found_message) => {
+        if (err) return next(err)
+        // message already exists redirect to message board.
+        if (found_message) res.redirect('/')
+        else {
+          // all ok, attempt saving to db
+          message.save((err) => {
+            if (err) return next(err)
+            // Message saved. Redirect to message board page.
+            res.redirect('/')
+          })
+        }
+      })
+    }
+  }
+];
 
+// Display message edit form on POST.
+exports.edit_get = (req, res, next) => {
+  Message.findOne({"username": req.user.username}).exec((err, message) =>{
+    res.render("message_form", {
+      title: "Edit Message",
+      data: { name: req.user.username, message: message.message }
+    })
+  })
+};
+
+// Handle message edit on POST.
+exports.edit_post = [
+  // Validate and sanitize the name field.
+  body("message", "at least 2 characters required").isLength({ min: 2 }),
+  body("message", "max 64 characters allowed").isLength({ max: 64 }),
+  body("message").custom((value, { req }) => {
+    if (customFilter.isProfane(value)) return false;
+    // regex match against the list of bad words
+    const does_match = dirty_words.some(word => {
+      const regex = new RegExp(`\\s${word}\\s|mother.+|sister.+`, 'i')
+      return regex.test(value)
+    })
+    if (does_match) return false;
+    return true
+  }).withMessage("Your message can not contain bad words"),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req)
     // Find the user message object and edit message property.
-    Message.findOne({"username": req.user.username}).exec((err, message) => {
+    Message.findOne({"username": req.user.username})
+    .exec((err, message) => {
       message.message = req.body.message;
       if (!errors.isEmpty()) {
         // There are errors. Render the form again with sanitized values/error messages.
@@ -81,80 +133,26 @@ exports.edit_post = [
           method: 'POST',
           data: message,
           errors: errors.array(),
-        });
+        })
         return
       } else {
           // Data from form is valid, attempt saving to db
           message.save((err) => {
-            if (err) return next(err);
+            if (err) return next(err)
             // Message saved. Redirect to message board page.
-            res.redirect('/');
-          });
+            res.redirect('/')
+          })
         }
     })
-  },
-];
-
-// Handle message create on POST.
-exports.create_post = [
-  // Validate and sanitize the name field.
-  body("message", "at least 2 characters required").isLength({ min: 2 }),
-  body("message", "max 64 characters allowed").isLength({ max: 64 }),
-  body("message")
-  .custom((value, { req }) => {
-    if (customFilter.isProfane(value)) return false;
-    // regex match against the list of bad words
-    const does_match = dirty_words.some(word => {
-      const regex = new RegExp(`\\s${word}\\s|mother.+|sister.+`, 'i');
-      return regex.test(value)
-    });
-    if (does_match) return false;
-    return true;
-  }).withMessage("Your message can not contain bad words"),
-
-  // Process request after validation and sanitization.
-  (req, res, next) => {
-    // Extract the validation errors from a request.
-    const errors = validationResult(req);
-
-    // Create a message object with escaped and trimmed data.
-    const message = new Message({ username: req.user.username, message: req.body.message });
-
-    if (!errors.isEmpty()) {
-      // There are errors. Render the form again with sanitized values/error messages.
-      res.render("message_form", {
-        title: "Create Message",
-        method: 'POST',
-        data: message,
-        errors: errors.array(),
-      });
-      return
-    } else {
-      // Data from form is valid.
-      // Check if message with same name and message already exists.
-      Message.findOne({ username: req.user.username, message: req.body.message })
-        .exec((err, found_message) => {
-          if (err) return next(err);
-          if (found_message) res.redirect('/'); // redirect to message board.
-          else {
-            // all ok, attempt saving to db
-            message.save((err) => {
-              if (err) return next(err);
-              // Message saved. Redirect to message board page.
-              res.redirect('/');
-            });
-          }
-        });
-    }
-  },
+  }
 ];
 
 exports.delete_post = (req, res, next) => {
   if (req.params.id === "") return res.status(204).send() // avoid default on cancel
   Message.deleteOne({_id: req.params.id})
-    .exec((err, message) => {
-      if (err) return next(err);
-      res.status(204).send() // avoid redirection after deleting
+  .exec((err, message) => {
+    if (err) return next(err)
+    res.status(204).send() // avoid redirection after deleting
 
       //Successful, so render
       //console.log("user:", user)
@@ -163,5 +161,5 @@ exports.delete_post = (req, res, next) => {
       //   messages,
       //   user: req.user
       // });
-    })
-};
+  })
+}
